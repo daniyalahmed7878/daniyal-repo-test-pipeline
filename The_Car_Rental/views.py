7e1fdeb0980d7d1bbac6005bd178d674353581a9
Daniyal Ahmed Sethi <daniyalahmedsethi@gmail.com>
2024-07-19 11:22:36 +0500

    test on own repo

---
+++ The_Car_Rental/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+
+
+
+
+
+def homePage(request):
+    return render(request, 'index.html')
+
+def aboutPage(request):
+    return render(request, 'about.html') 
+
+
+
+def loginPage(request):
+    return render(request, 'login.html') 
+
+
+
+def Create_accountPage(request):
+    return render(request, 'create_account.html') 
+
+
+
+def faqPage(request):
+    return render(request, 'faq.html') 
